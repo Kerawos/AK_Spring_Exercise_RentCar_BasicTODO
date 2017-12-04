4a4c64ab943ecc2688bd4b5a5db9e5e633fd7df5
Kerawos <Maresowa@gmail.com>
2017-12-04 11:52:16 +0100

    @add controllers descriptions

--- src/main/java/pl/akademiakodu/AK_Spring_Exercise_RentCar_BasicTODO/controllers/DescController.java
+++ src/main/java/pl/akademiakodu/AK_Spring_Exercise_RentCar_BasicTODO/controllers/DescController.java
@@ -1,12 +1,20 @@
 package pl.akademiakodu.AK_Spring_Exercise_RentCar_BasicTODO.controllers;
-
+/**
+ * Import Section
+ */
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+/**
+ * Control description
+ */
 @Controller
 public class DescController {
 
+    /**
+     * Method to get description image from description template to user
+     * @return description predefined template containing description of exercise
+     */
     @GetMapping("/desc")
     public String descGet(){
         return "desc";
--- src/main/java/pl/akademiakodu/AK_Spring_Exercise_RentCar_BasicTODO/controllers/IndexController.java
+++ src/main/java/pl/akademiakodu/AK_Spring_Exercise_RentCar_BasicTODO/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package pl.akademiakodu.AK_Spring_Exercise_RentCar_BasicTODO.controllers;
 
+/**
+ * Import section
+ */
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,22 +18,37 @@ import pl.akademiakodu.AK_Spring_Exercise_RentCar_BasicTODO.models.bags.UserPref
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Control main page with exercise
+ */
 @Controller
 public class IndexController {
 
-    //declarations
+    /**
+     * Declaration of variables
+     */
     OptimalCarFounder optimalCarFounder;
     ResultToUser resultToUser;
     List<CarModel> potentialRentCarList;
     List<ResultModel> resultModelList;
     StringBuilder sb;
 
-
+    /**
+     * Method get user inputs and moving them to post method
+     * @param model stored user inputs
+     * @return filled user input model
+     */
     @GetMapping("/") public String getIndex(Model model){
         model.addAttribute("userPreferencesModel", new UserPreferencesModel());
         return "index";
     }
 
+    /**
+     * Method managing given user inputs and showing to user calculated results
+     * @param userPreferencesModel given user inputs
+     * @param model to display messages to user
+     * @return calculated results. Potential rent car costs.
+     */
     @PostMapping("/") public String postIndex(@ModelAttribute UserPreferencesModel userPreferencesModel, Model model){
 
         //initializations
